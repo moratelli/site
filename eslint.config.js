bf9786efe463166df8918635112a2099aee34a6e
Pedro Moratelli <pedromoratelli@gmail.com>
2025-11-18 21:55:09 -0300

    feat: setup github action

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import js from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
 
 export default defineConfig([
   globalIgnores(['dist']),
